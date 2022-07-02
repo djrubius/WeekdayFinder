edec64d2d62a4013f1928f1a8d22971a4fde372a
Dj_RuBiuS <sarubius@gmail.com>
2022-07-02 21:09:31 +0300

    Adding code logic for app

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -15,9 +15,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func findDay(){
+    @IBAction func findWeekDay(){
         
+        let calendar = Calendar.current
         
+        var dateComponents = DateComponents()
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else {return}
+        
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponents) else {return}
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekDay = dateFormatter.string(from: date)
+        let capitalizedWeekDay = weekDay.capitalized
+        
+        resultLabel.text = capitalizedWeekDay
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
